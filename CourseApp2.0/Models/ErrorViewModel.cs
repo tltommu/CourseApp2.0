aa185fee3d9c334f245a5d9a385c76fd116738e5
tltommu <tltommu527@gmail.com>
2025-04-01 23:25:27 +0800

    Add project files.

---
+++ CourseApp2.0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CourseApp2._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
