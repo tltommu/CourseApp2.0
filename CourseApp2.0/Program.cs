327616fedcb24e3d4357109b3951842ac49fdae9
tltommu <tltommu527@gmail.com>
2025-04-04 01:07:13 +0800

    Adding google auth to the app, and connecting a database

--- CourseApp2.0/Program.cs
+++ CourseApp2.0/Program.cs
@@ -1,6 +1,7 @@
 using CourseApp2._0.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Google.Apis.Auth.AspNetCore3;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
